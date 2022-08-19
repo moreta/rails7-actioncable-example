15cc6e6a5abdd6ebf054bb7d5188fe941f7c0777
jungjoo.seo <jj@brakio.com>
2022-08-19 15:57:30 +0900

    chat with actioncable

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "rooms#show"
+  get 'rooms/show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+
 end
